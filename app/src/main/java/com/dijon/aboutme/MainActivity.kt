454f547c0399944e1221826e72681c6973831bba
dijoncavalcante <dijoncavalcante@gmail.com>
2021-03-31 22:49:38 -0400

    chore:Trying to call Detail Activity

--- app/src/main/java/com/dijon/aboutme/MainActivity.kt
+++ app/src/main/java/com/dijon/aboutme/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.dijon.aboutme
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val iv_experience =findViewById<ImageView>(R.id.iv_experience)
+        iv_experience.setOnClickListener {
+            val intent = Intent(this, ExperienceActivity::class.java).apply {
+                startActivity(intent)
+            }
+        }
     }
+
+
 }
