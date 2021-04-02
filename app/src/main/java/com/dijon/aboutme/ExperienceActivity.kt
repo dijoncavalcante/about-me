8f3ffc4e6d59d0d6c0feafdc5e875454404c9250
dijoncavalcante <dijoncavalcante@gmail.com>
2021-04-02 18:14:58 -0400

    feature: Add back button. color of the Toolbar changed[title]

--- app/src/main/java/com/dijon/aboutme/ExperienceActivity.kt
+++ app/src/main/java/com/dijon/aboutme/ExperienceActivity.kt
@@ -2,10 +2,19 @@ package com.dijon.aboutme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dijon.aboutme.databinding.ActivityExperienceBinding
+import com.dijon.aboutme.presentation.base.BaseActivity
 
-class ExperienceActivity : AppCompatActivity() {
+
+class ExperienceActivity : BaseActivity() {
+
+    lateinit var bindingExperience: ActivityExperienceBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_experience)
+        bindingExperience = ActivityExperienceBinding.inflate(layoutInflater)
+        setContentView(bindingExperience.root)
+
+         setupToolbar(bindingExperience.layoutInclude.toolbarMain, R.string.experience, true)
     }
 }
