83f05bd2f0e2d52a0513f33890d039e00bf6a2de
dijoncavalcante <dijoncavalcante@gmail.com>
2021-04-01 22:10:52 -0400

    chore: remove res/screenshot directory. Updating called ExperienceActivity

--- app/src/main/java/com/dijon/aboutme/MainActivity.kt
+++ app/src/main/java/com/dijon/aboutme/MainActivity.kt
@@ -6,18 +6,23 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import androidx.cardview.widget.CardView
+import com.dijon.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val iv_experience =findViewById<ImageView>(R.id.iv_experience)
-        iv_experience.setOnClickListener {
-            val intent = Intent(this, ExperienceActivity::class.java).apply {
-                startActivity(intent)
-            }
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.layoutRoot.carViewExperience.setOnClickListener { callExperience() }
+
     }
 
-
+    private fun callExperience() {
+        val intent = Intent(this, ExperienceActivity::class.java)
+        startActivity(intent)
+    }
 }
