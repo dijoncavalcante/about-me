b868efe38a95a5764908227050908edfad7568ff
dijoncavalcante <dijoncavalcante@gmail.com>
2021-04-11 18:17:37 -0400

    Add informations  about my person

--- app/src/main/java/com/dijon/aboutme/AboutMeActivity.kt
+++ app/src/main/java/com/dijon/aboutme/AboutMeActivity.kt
@@ -39,19 +39,17 @@ class AboutMeActivity : BaseActivity() {
     }
 
     private fun postToList() {
-        addToList("Qual é o meu nome?", "Meu nome é Dijon Cavalcante Braga.")
+        addToList("Qual é o meu nome?", "Eu sou Dijon Braga.")
         addToList("Qual é a minha idade?", "Eu tenho 30 anos de idade.")
-        addToList("Eu sou casado?", "SIM! Eu sou casado há 6 anos.")
-        addToList("Qual é a minha profissão?", "Atualmente eu sou Engenheiro de Software no Sidia Instituto de Ciência e Tecnologia, fica localizado em Manaus/Amazonas.")
+        addToList("Eu sou casado?", "SIM! Eu sou casado há 6 anos. Ainda não tenho filhos, mas desejo ter no momento certo.")
+        addToList("Qual é a minha profissão?", "Atualmente eu sou Engenheiro de Software no Sidia Instituto de Ciência e Tecnologia, fica localizado em Manaus/Amazonas/Brasil.")
         addToList("Em que eu sou graduado?", "Eu sou Baicharel em Ciência da Computação.")
-        addToList("Onde eu nasci?", "Eu nasci em Manaus/Amazonas")
-        addToList("Qual é o meu nome?", "Meu")
-        addToList("Qual é o meu nome?", "Meu")
-        addToList("Qual é o meu nome?", "Meu")
-        addToList("Qual é o meu nome?", "Meu")
-        addToList("Qual é o meu nome?", "Meu")
-        addToList("Qual é o meu nome?", "Meu")
-        addToList("Qual é o meu nome?", "Meu")
+        addToList("Onde eu nasci?", "Eu nasci em Manaus/Amazonas/Brasil.")
+        addToList("Qual meu hobby?", "Bem, eu gosto de jogar video game, gosto de futebol também.")
+        addToList("Quais linguagens de programação eu ja trabalhei?", "Atualmente eu trabalho com Java atuando dentro do campo de android embarcado, mas eu iniciei trabalhando com Java EE, logo migrei para .Net[desktop], fiz muitas muitas conversões de tela de Visual Basic 6(VB6) para .Net e criei muitos sistemas diretamente com .Net")
+        addToList("Como eu me vejo daqui a 5 anos?", "Daqui a 5 anos eu desejo ser especialista em desenvolvimento de Aplicativos Móveis.")
+        addToList("Qual meu nível de Inglês?", "Bem, eu diria que estou no nível intermediário. Consigo ler, ouvir e me comunicar. Atualmente estou estudando na Minds School. Qualquer coisa me avisa que te indico lá para garantir um bônus para mim kkkk...")
+        addToList("Meus contatos?", "Segue meu número de celular: 5592993210558.\nMeu email: dijoncavalcante@gmail.com\nPerfil do linkedIn: https://www.linkedin.com/in/dijon-braga-9a5b3911a\nO restanto consulte a Página Me Encontre. Lá você vai ter mais opções de me encontrar.")
     }
 
 }
