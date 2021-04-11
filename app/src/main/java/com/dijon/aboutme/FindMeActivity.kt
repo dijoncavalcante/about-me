e9598aa28c603bbcc5dfe371985269cf1a8d19f3
dijoncavalcante <dijoncavalcante@gmail.com>
2021-04-11 15:41:25 -0400

    feature: Open profile on FindMe Layout

--- app/src/main/java/com/dijon/aboutme/FindMeActivity.kt
+++ app/src/main/java/com/dijon/aboutme/FindMeActivity.kt
@@ -18,29 +18,12 @@ class FindMeActivity : BaseActivity() {
 
         setupToolbar(bindingFindMeActivity.layoutInclude.toolbarMain, R.string.findMe, true)
 
-        bindingFindMeActivity.carViewFindMeWpp.setOnClickListener { openWhatsApp() }
         bindingFindMeActivity.carViewFindMeCall.setOnClickListener { openCaller() }
-
-
-    }
-
-    private fun openCaller() {
-        val intent = Intent(Intent.ACTION_DIAL)
-        intent.data = Uri.parse("tel:${getString(R.string.myCelphoneNumberwithDDD)}")
-        startActivity(intent)
-    }
-
-    private fun openWhatsApp() {
-        val isAppInstalled = appInstalledOrNot("com.whatsapp")
-        if (isAppInstalled) {
-            val openIntent = Intent(
-                Intent.ACTION_VIEW,
-                Uri.parse("https://api.whatsapp.com/send?phone=${getString(R.string.myCelphoneNumberwithDDD)}&text=${getString(R.string.message_hi_findme)}")
-            )
-            startActivity(openIntent)
-        } else {
-            // WhatsApp not installed show toast or dialog
-        }
+        bindingFindMeActivity.carViewFindMeWpp.setOnClickListener { openWhatsApp() }
+        bindingFindMeActivity.carViewFindMeLinkedin.setOnClickListener { openLinkedIn() }
+        bindingFindMeActivity.carViewFindMeGithub.setOnClickListener { openGithub() }
+        bindingFindMeActivity.carViewFindMeTwitter.setOnClickListener { openTwitter() }
+        bindingFindMeActivity.carViewFindMeInstagram.setOnClickListener { openIntagran() }
     }
 
     private fun appInstalledOrNot(uri: String): Boolean {
@@ -52,4 +35,62 @@ class FindMeActivity : BaseActivity() {
             false
         }
     }
+
+    private fun openCaller() {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse("tel:${getString(R.string.myCelphoneNumberwithDDD)}")
+        startActivity(intent)
+    }
+
+    private fun openWhatsApp() {
+        val isAppInstalled = appInstalledOrNot("com.whatsapp")
+        if (isAppInstalled) {
+            val openIntent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse(
+                    "https://api.whatsapp.com/send?phone=${getString(R.string.myCelphoneNumberwithDDD)}&text=${
+                        getString(
+                            R.string.message_hi_findme
+                        )
+                    }"
+                )
+            )
+            startActivity(openIntent)
+        } else {
+            // WhatsApp not installed show toast or dialog
+        }
+    }
+
+    private fun openLinkedIn() {
+        val openIntent = Intent(
+            Intent.ACTION_VIEW,
+            Uri.parse("https://www.linkedin.com/in/dijon-braga-9a5b3911a")
+        )
+        startActivity(openIntent)
+    }
+
+    private fun openGithub() {
+        val openIntent = Intent(
+            Intent.ACTION_VIEW,
+            Uri.parse("https://github.com/dijoncavalcante")
+        )
+        startActivity(openIntent)
+    }
+
+    private fun openTwitter() {
+        val openIntent = Intent(
+            Intent.ACTION_VIEW,
+            Uri.parse("https://twitter.com/DijonBraga")
+        )
+        startActivity(openIntent)
+    }
+
+    private fun openIntagran() {
+        val openIntent = Intent(
+            Intent.ACTION_VIEW,
+            Uri.parse("https://www.instagram.com/dijonbraga/")
+        )
+        startActivity(openIntent)
+    }
+
 }
